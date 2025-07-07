ca03b62b9ab81c01c88c087e32c59ea4041ecad5
Maksimcode <max.shevtsov1@gmail.com>
2025-07-07 16:31:06 +0700

    Splash Screen and app logo added

--- src/main/java/com/example/pizza_shevtsov_shift_2025/StartActivity.kt
+++ src/main/java/com/example/pizza_shevtsov_shift_2025/StartActivity.kt
@@ -3,6 +3,7 @@ package com.example.pizza_shevtsov_shift_2025
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.pizza_shevtsov_shift_2025.navigation.SetupNavGraph
@@ -14,6 +15,7 @@ class StartActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             Pizzashevtsovshift2025Theme {
                 navController = rememberNavController()
